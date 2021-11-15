205d4a9e3c7b4aac26a355f059d4721651891196
FedeAdra25 <fedeadra@gmail.com>
2021-11-15 00:08:21 -0300

    Firmware update. Finished.

--- Firmware/STM32F103C6/main.c
+++ Firmware/STM32F103C6/main.c
@@ -8,39 +8,32 @@
 #include <main.h>
 
 
-
-
-
 volatile uint32_t capacitance=0,inductance=0; //capacitance and inductance in uF and uH
-char str[10];
 int main (void)
  { 
    // RCC first cuz simulation crashes otherwise :(
 	RCC->APB2ENR|= 0xFC | (1<<9) | (1<<14) | (1<<11); //enable clock for GPIO, ADC1 clock, usart1 and TIM1.
-	RCC->APB2ENR|= (1<<11); //enable TIM2 (L measure uses it)
+	RCC->APB1ENR|= (1<<11); //enable TIM2 (L measure uses it)
 
 	System_Init();
 	LCDinit();
 	usart1_init();
 	ADC_Init();
 	TIMER1_Init();
-
-
-	LCDclr();
-	LCDGotoXY(0,0);	
-	
+	LCDclr();	
    while (1){
-		delay_ms(100);
+		LCDGotoXY(0,0);
+		//delay_ms(100);
 		capacitance = System_MeasureC();
 		LCDstring("C= ",3);
 		LCDSendInt(capacitance);
-		LCDstring("nF (+-1%)", 9);
+		LCDstring("nF", 2); // 1% error aprox
 		LCDGotoXY(0,1);
-		delay_ms(100);
+		//delay_ms(100);
 		inductance = System_MeasureL();
 		LCDstring("F= ", 3);
 		LCDSendInt(inductance);
-		LCDstring("uH (+-5%)", 9);
+		LCDstring("uH", 2); //10% error aprox
 	}
    return 0;
  }   
--- Firmware/STM32F103C6/system_measure.c
+++ Firmware/STM32F103C6/system_measure.c
@@ -3,21 +3,18 @@
 
 //RC_FACTOR: R*C=T --> C = T/RC_FACTOR, T[us] and C[nF] for R=10Kohm
 #define RC_FACTOR 10
+#define PI 3.14159265358
+#define C_VALUE 10
 
 
 
-
-volatile uint32_t temp=0,capacitanceM=0;
+volatile uint32_t temp=0,capacitanceM=0,inductanceL;
+volatile float measures[30],L;
 
 void System_Init(void){
-
-
 	GPIOB->CRL = 0x44444443; //configure PB0 as output
 
-	//Configuration for measuring C
-	
-	
-	
+	//Configuration for measuring C	
 
 	//Configuration for measuring L
 	//Tim2 config
@@ -39,7 +36,7 @@ uint32_t System_MeasureC(void){
 	while(1){ //while Vc<trigger
 		temp=ADC_GetInputInmV();
 		if(temp>=2086){
-			capacitanceM=(TIMER1_GetValue()*1000000) / (F_CLK_TIM1); //time in us
+			capacitanceM=(TIMER1_GetValue())*(1000000UL/(F_CLK_TIM1)); //time in us
 			capacitanceM=capacitanceM/RC_FACTOR;
 			usart1_sendStr("Stopped at [mv] -> ");
 			usart1_sendInt(temp);
@@ -54,26 +51,41 @@ uint32_t System_MeasureC(void){
 
 }
 uint32_t System_MeasureL(void){
-	volatile uint32_t measures[30], inductanceL=0,i,t=0,t0=0,T=0,f;
-
+	volatile uint32_t i,t=0,t0=0,T=0,min,max,imin,imax;
 	GPIOB->BSRR=(1<<0); //start charging LC circuit
-	delay_ms(200); //wait a little
+	delay_ms(300); //wait a little
 	GPIOB->BSRR=(1<<16); //bsrr set to 0, now we meassure L
 	TIM2->CR1 = 1;/* start counting up  ACTIVATE TIM2*/
 	for(i=0;i<30;i++){
 		while((TIM2->SR &(1<<3)) == 0); /* wait until the CC3IF flag sets */
 		t = TIM2->CCR3;/* read the captured value */
 		T = t-t0; //t0=0
-		f=10000UL/T;
 		t0=t;
-		measures[i]=f; //save measure in array
+		measures[i]=T; //save T measure in array
 	}
-	
-	for(i=1;i<29;i++){
-		inductanceL=inductanceL+measures[i];
+	//Filter out minimum and maximum (excluding first and last 5 measures)
+	min=measures[0];
+	max=measures[0];
+	for(i=5;i<24;i++){
+		if(measures[i]<min){
+			min=measures[i];
+			imin=i;
+		}
+		if(measures[i]>max){
+			max=measures[i];
+			imax=i;
+		}
 	}
-	inductanceL=inductanceL/28; 
-	usart1_sendStr("Value of frequency:  ");
+	//Filter out first and last 5 measures
+	for(i=5;i<24;i++){ //skip first 5 measures and last 5, minimum and maximum value
+		if(i==imin || i==imax)
+			continue;
+		L=(measures[i]*measures[i])/(PI*PI*4*C_VALUE); //calculate L
+		inductanceL=inductanceL+(uint32_t)L;
+	}
+
+	inductanceL=inductanceL/18; 
+	usart1_sendStr("Value of Inductance:  ");
 	usart1_sendInt((uint16_t)inductanceL); /* send the difference */
 	usart1_sendStr("\n\r"); /* go to new line */		
 	return inductanceL;
